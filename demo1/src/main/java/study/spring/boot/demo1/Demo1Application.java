829156f951c1a8f5aebfe7f89a8713dacdfd9530
김윤수 <younsookim@tmon.co.kr>
2019-11-20 01:43:03 +0900

    자동 설정 만들기 1부: Starter와 AutoConfigure

--- demo1/src/main/java/study/spring/boot/demo1/Demo1Application.java
+++ demo1/src/main/java/study/spring/boot/demo1/Demo1Application.java
@@ -6,19 +6,24 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import study.spring.boot.Holoman;
 
 @SpringBootApplication
-// 위의 하나의 어노테이션과 아래 3개의 어노테이션 사용은 동일하다.
-//@SpringBootConfiguration
-//@ComponentScan
-//@EnableAutoConfiguration
 public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Demo1Application.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
-		//SpringApplication.run(Demo1Application.class, args);
 	}
 
+	@Bean
+	public Holoman holoman() {
+		Holoman holoman = new Holoman();
+		holoman.setName("whiteship");
+		holoman.setHowLong(60);
+		return holoman;
+	}
 }
