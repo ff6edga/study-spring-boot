2649561eb7410467623c02bccd87bfc123cb037e
김윤수 <younsookim@tmon.co.kr>
2019-11-22 15:33:31 +0900

    외부설정 2부

--- demo2/src/main/java/study/spring/demo2/Demo2Runner.java
+++ demo2/src/main/java/study/spring/demo2/Demo2Runner.java
@@ -1,6 +1,6 @@
 package study.spring.demo2;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,19 +8,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class Demo2Runner implements ApplicationRunner {
 
-	@Value("${younsoo.fullName}")
-	private String name;
-
-	@Value("${younsoo.age}")
-	private Integer age;
-
-
+	@Autowired
+	YounsooProperties props;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("=====================");
-		System.out.println(name);
-		System.out.println(age);
+		System.out.println(props.getName());
+		System.out.println(props.getAge());
+		System.out.println(props.getSessionTimeout());
 		System.out.println("=====================");
 	}
 }
