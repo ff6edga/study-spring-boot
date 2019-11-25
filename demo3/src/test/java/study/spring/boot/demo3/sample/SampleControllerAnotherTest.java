f77e553297db9319ffe53d9b173d7e96beb02075
김윤수 <younsookim@tmon.co.kr>
2019-11-25 12:39:15 +0900

    테스트 유틸

--- demo3/src/test/java/study/spring/boot/demo3/sample/SampleControllerAnotherTest.java
+++ demo3/src/test/java/study/spring/boot/demo3/sample/SampleControllerAnotherTest.java
@@ -1,10 +1,12 @@
 package study.spring.boot.demo3.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -15,6 +17,9 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerAnotherTest {
+
+	@Rule
+	public OutputCapture outputCapture = new OutputCapture();
 
 	@Autowired
 	TestRestTemplate testRestTemplate;
@@ -40,5 +45,9 @@ public class SampleControllerAnotherTest {
 		webTestClient.get().uri("/hello").exchange()
 				.expectStatus().isOk()
 				.expectBody(String.class).isEqualTo("hello younsoo");
+
+		assertThat(outputCapture.toString())
+				.contains("holoman")
+				.contains("skip");
 	}
 }
