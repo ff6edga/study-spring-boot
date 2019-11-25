d9799365d8f520f716c9e4ca04fae874109c8a08
김윤수 <younsookim@tmon.co.kr>
2019-11-26 01:00:39 +0900

    스프링 웹 MVC : ViewResolve

--- demo-mvc/src/test/java/study/spring/boot/demomvc/user/UserControllerTest.java
+++ demo-mvc/src/test/java/study/spring/boot/demomvc/user/UserControllerTest.java
@@ -14,9 +14,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -44,5 +43,21 @@ public class UserControllerTest {
 					is(equalTo("younsoo"))))
 			.andExpect(jsonPath("password",
 					is(equalTo("1234"))));
+	}
+
+	@Test
+	public void createUser_XML() throws Exception{
+		String userJson = "{\"username\":\"younsoo\", \"password\":\"1234\"}";
+		mockMvc.perform(post("/users/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_XML)
+				.content(userJson))
+				// 에러가 발생하면 자동으로 실행된다.
+				//.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(xpath("/User/username")
+						.string("younsoo"))
+				.andExpect(xpath("/User/password")
+						.string("1234"));
 	}
 }
