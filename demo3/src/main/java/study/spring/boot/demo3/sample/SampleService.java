ac0a37a3ac87841cfb22c6d35ca1564993fee78b
김윤수 <younsookim@tmon.co.kr>
2019-11-25 12:27:56 +0900

    테스트

---
+++ demo3/src/main/java/study/spring/boot/demo3/sample/SampleService.java
@@ -0,0 +1,11 @@
+package study.spring.boot.demo3.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+	public  String getName() {
+		return "younsoo";
+	}
+}
