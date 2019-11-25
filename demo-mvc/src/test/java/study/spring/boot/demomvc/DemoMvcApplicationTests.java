c06c0340de9418c9a6bacda14c85fc1533a67e06
김윤수 <younsookim@tmon.co.kr>
2019-11-25 16:21:23 +0900

    스프링 웹 MVC 소개

---
+++ demo-mvc/src/test/java/study/spring/boot/demomvc/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.boot.demomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
