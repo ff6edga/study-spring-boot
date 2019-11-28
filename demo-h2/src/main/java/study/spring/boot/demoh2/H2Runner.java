37e58f312ca915dc531349e6cf5fb6d93fb28fe0
김윤수 <younsookim@tmon.co.kr>
2019-11-28 15:33:58 +0900

    스프링 데이터 - 인메모리 데이터베이스

---
+++ demo-h2/src/main/java/study/spring/boot/demoh2/H2Runner.java
@@ -0,0 +1,38 @@
+package study.spring.boot.demoh2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+	@Autowired
+	DataSource dataSource;
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		try(Connection connection = dataSource.getConnection()) {
+			System.out.println(connection.getMetaData().getURL());
+			System.out.println(connection.getMetaData().getUserName());
+
+			Statement statement = connection.createStatement();
+			String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY\n"
+					+ "KEY (id))";
+			statement.executeUpdate(sql);
+		}
+		// JdbcTemplate가 훨씬 간결하게 SQL을 사용할 수 있고, 좀 더 안전하게
+		// 사용할 수 있으며 Resource 반납 처리도 잘 되어 있으며
+		// 훨씬 가독성 좋은 예외 처리를 할 수 있다.
+		jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'younsoo')");
+	}
+}
---
+++ demo-h2/src/test/java/study/spring/boot/demoh2/DemoH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.boot.demoh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
