008684470b845b8ebf7ab489805646e83794639a
김윤수 <younsookim@tmon.co.kr>
2019-11-28 17:48:52 +0900

    스프링 데이터 - MySQL

--- demo-h2/src/main/java/study/spring/boot/demoh2/MySQLRunner.java
+++ demo-h2/src/main/java/study/spring/boot/demoh2/MySQLRunner.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class H2Runner implements ApplicationRunner {
+public class MySQLRunner implements ApplicationRunner {
 
 	@Autowired
 	DataSource dataSource;
@@ -22,17 +22,18 @@ public class H2Runner implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		try(Connection connection = dataSource.getConnection()) {
+			System.out.println(connection.getClass()); // 어떤 DBCP를 쓰는지 확인
 			System.out.println(connection.getMetaData().getURL());
 			System.out.println(connection.getMetaData().getUserName());
 
-			Statement statement = connection.createStatement();
-			String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY\n"
-					+ "KEY (id))";
-			statement.executeUpdate(sql);
+//			Statement statement = connection.createStatement();
+//			String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY\n"
+//					+ "KEY (id))";
+//			statement.executeUpdate(sql);
 		}
 		// JdbcTemplate가 훨씬 간결하게 SQL을 사용할 수 있고, 좀 더 안전하게
 		// 사용할 수 있으며 Resource 반납 처리도 잘 되어 있으며
 		// 훨씬 가독성 좋은 예외 처리를 할 수 있다.
-		jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'younsoo')");
+//		jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'younsoo')");
 	}
 }
