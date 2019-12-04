e8b40c1928dd830b4c4379b0a5946c611127ca50
김윤수 <younsookim@tmon.co.kr>
2019-12-04 23:56:04 +0900

    스프링 REST 클라이언트 - RestTemplate와 WebClient

---
+++ demo-rest/src/main/java/study/spring/boot/demorest/DemoRestApplication.java
@@ -0,0 +1,13 @@
+package study.spring.boot.demorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestApplication.class, args);
+	}
+
+}
